f865b3ecbecf128f102a408bf8b133104ee8901b
Braiden Peterson <braidensp@me.com>
2018-01-17 23:06:48 -0800

    Finish Mozilla JS basics

--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,22 @@ myImage.onclick = function() {
   }
 }
 
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
 
-/*
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-    }
+function setUserName() {
+  var myName = prompt('Please enter your name');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Web development is cool, ' + myName;
 }
-*/
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Web development is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
