3fdd21f460d173cdea9fb53696ab2eae83cb8d28
Braiden Peterson <braidensp@me.com>
2018-01-17 22:19:59 -0800

    Fix JS error

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/character-01.png'); {
+  if (mySrc === 'images/character-01.png') {
     myImage.setAttribute ('src','images/character-02.png');
   } else {
     myImage.setAttribute ('src','images/character-01.png');
